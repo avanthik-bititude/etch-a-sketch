18e4a442111118e57877ffaf07085ea1185de1b0
avanthik <avanthik@bititude.com>
2024-09-26 17:15:50 +0530

    changes made

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 function createGrid(row) {
   const container = document.getElementById("container");
-  container.style.width = `${30 * row}px`; 
-  container.style.height = `${30 * row}px`; 
-  container.innerHTML=''
+  container.style.width = `${20 * row}px`;
+  container.style.height = `${20 * row}px`;
+  let isClicked = false;
+  container.innerHTML = "";
+
   for (let i = 0; i < row * row; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
+    square.addEventListener("mouseover", () => {
+      square.classList.add("painted");
+    });
     container.appendChild(square);
   }
 }
 
 function readInput() {
-  const grid = prompt("Number of grids Lessthan 100");
+  const grid = prompt("Number of grids (Less than 100)");
   createGrid(parseInt(grid));
 }
