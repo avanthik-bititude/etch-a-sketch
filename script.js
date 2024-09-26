84864d48e897cb26d5a23737f1eacd0f75af50f9
avanthik <avanthik@bititude.com>
2024-09-26 11:46:20 +0530

    created new repo

---
+++ script.js
@@ -0,0 +1,16 @@
+function createGrid(row) {
+  const container = document.getElementById("container");
+  container.style.width = `${30 * row}px`; 
+  container.style.height = `${30 * row}px`; 
+  container.innerHTML=''
+  for (let i = 0; i < row * row; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    container.appendChild(square);
+  }
+}
+
+function readInput() {
+  const grid = prompt("Number of grids");
+  createGrid(parseInt(grid));
+}
